18676eab76879c56dce0a5410d0e538d19ad27f9
Ogishima kaede <a5816017@aoyama.jp>
2018-06-24 13:51:52 +0900

    pythin

--- pythontest/p9_3.py
+++ pythontest/p9_3.py
@@ -5,10 +5,10 @@ labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 num_bars = len(steps)
 positions = range(1, num_bars+1)
 
-plt.barh(positions, steps, align='center')
-plt.yticks(positions, labels)
-plt.xlabel('Steps')
-plt.ylabel('Day')
+plt.bar(positions, steps, align='center')
+plt.xticks(positions, labels)
+plt.ylabel('Steps')
+plt.xlabel('Day')
 plt.title('Number of steps walked')
 
 plt.grid()
